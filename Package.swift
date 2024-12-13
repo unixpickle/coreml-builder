7cc44ecbe6a7271d897f72593bf053fe9f2e6fb3
Alex Nichol <unixpickle@gmail.com>
2024-12-12 23:26:18 -0500

    ios dep

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CoreMLBuilder",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v13), .iOS(.v16)],
     products: [
         .library(
             name: "CoreMLProto",
